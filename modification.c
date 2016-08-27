ed92ee8cc7c53ff04326b33cc9bb424b5cb1680f
DaliChou <fatigba72@gmail.com>
2016-08-27 21:08:22 +0000

    Update modification.c

--- modification.c
+++ modification.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
-#include <float.h>
-#include <math.h>
+
+// Fonction: setPixel
+// Synopsis: fonction permettant de récupérer une pixel
+// parm:
+// - surface, la surface source
+// - x, coordonnée en abcsisse de la pixel
+// - y, coordonnée en ordonnée de la pixel
 
 static
 Uint32 getPixel(SDL_Surface *surface, Uint32 x, Uint32 y)
@@ -32,6 +37,14 @@ Uint32 getPixel(SDL_Surface *surface, Uint32 x, Uint32 y)
     }
     return 0 ;
 }
+
+// Fonction: setPixel
+// Synopsis: fonction permettant de modifier une pixel
+// parm:
+// - surface, la surface source
+// - x, coordonnée en abcsisse de la pixel
+// - y, coordonnée en ordonnée de la pixel
+// - pixel, pixel à mettre sur la surface
 
 static
 void setPixel(SDL_Surface *surface, Uint32 x, Uint32 y, Uint32 pixel)
@@ -68,6 +81,12 @@ void setPixel(SDL_Surface *surface, Uint32 x, Uint32 y, Uint32 pixel)
     }
 }
 
+// Fonction: addVal
+// Synopsis: fonction permettant de modifier une composante de couleur
+// parm:
+// - cp composante à modifer
+// - val, valeur à ajouter ou à enlever
+
 static
 Uint8 addVal(Uint8 cp, int val)
 {
@@ -82,7 +101,16 @@ Uint8 addVal(Uint8 cp, int val)
     return (Uint8) tmp ;
 }
 
-static void forint(SDL_Surface *src, int x, int y, int dx, int dy, int _r, int _g, int _b)
+// Fonction: forint
+// Synopsis: fonction appelée par RGB et RGB_Mouse
+// parm:
+// - *src, pointeur sur la surface à traiter
+// x, y, dx, dy délimitent une un rectangle sur l'image à traiter
+// - _g, quantité de vert à ajouter ou à enlever
+// - _r, quantité de rouge à ajouter ou à enlever
+// - _b, quantité de bleu à ajouter ou à enlever
+static
+void forint(SDL_Surface *src, int x, int y, int dx, int dy, int _r, int _g, int _b)
 {
     Uint8 r = 0, g = 0, b = 0, a = 0 ;
     Uint32 color ;
@@ -102,6 +130,15 @@ static void forint(SDL_Surface *src, int x, int y, int dx, int dy, int _r, int _
     }
 }
 
+// Fonction: RGB_mouse
+// Synopsis: fonction permettant de modifier le balancement des couleurs pour une image en un point
+// parm:
+// - *src, pointeur sur la surface à traiter
+// - x, coordonnée en abcsisse de la souris
+// - y, coordonnée en ordonnée de la souris
+// - _g, quantité de vert à ajouter ou à enlever
+// - _r, quantité de rouge à ajouter ou à enlever
+// - _b, quantité de bleu à ajouter ou à enlever
 void RGB_Mouse(SDL_Surface *src, int x, int y, int _r, int _g, int _b)
 {
     SDL_LockSurface(src) ;
@@ -117,6 +154,13 @@ void RGB_Mouse(SDL_Surface *src, int x, int y, int _r, int _g, int _b)
     SDL_UnlockSurface(src) ;
 }
 
+// Fonction: RGB
+// Synopsis: fonction permettant de modifier le balancement des couleurs pour une image
+// parm:
+// - *src, pointeur sur la surface à traiter
+// - _g, quantité de vert à ajouter ou à enlever
+// - _r, quantité de rouge à ajouter ou à enlever
+// - _b, quantité de bleu à ajouter ou à enlever
 void RGB(SDL_Surface *src, int _r, int _g, int _b)
 {
     SDL_LockSurface(src) ;
@@ -124,16 +168,32 @@ void RGB(SDL_Surface *src, int _r, int _g, int _b)
     SDL_UnlockSurface(src) ;
 }
 
+// Fonction: luminosite
+// Synopsis: fonction permettant de modifier la luminosité d'un image
+// parm:
+// - *src, pointeur sur la surface à traiter
+// - _grey, quantité de couleur à ajouter ou à enlever
 void lumisote(SDL_Surface* src, int _grey)
 {
     RGB(src, _grey, _grey, _grey) ;
 }
 
+// Fonction: luminositeMouse
+// Synopsis: fonction permettant de modifier la luminosité d'un image en un point avec la souris
+// parm:
+// - *src, pointeur sur la surface à traiter
+// - x, coordonnée en abcsisse de la souris
+// - y, coordonnée en ordonnée de la souris
+// - _grey, quantité de couleur à ajouter ou à enlever
 void lumisoteMouse(SDL_Surface *src, int x, int y, int _grey)
 {
     RGB_Mouse(src, x, y, _grey, _grey, _grey) ;
 }
 
+// Fonction: greyLevel
+// Synopsis: fonction permettant la mise en niveau de gris d'une image
+// parm:
+// - *src, pointeur sur la surface à traiter
 void greyLevel(SDL_Surface *src)
 {
     Uint8 r, g, b ;
@@ -155,6 +215,11 @@ void greyLevel(SDL_Surface *src)
     SDL_UnlockSurface(src) ;
 }
 
+// Fonction: addMasque
+// Synopsis: fonction permettant d'appliquer un masque alpha sur une image
+// parm:
+// - *src, pointeur sur la surface à traiter
+// - *masque, pointeur sur la surface servant de masque
 bool addMasque(SDL_Surface *src, SDL_Surface *masque)
 {
     if(src->w != masque->w || src->h != masque->h)
@@ -189,6 +254,10 @@ bool addMasque(SDL_Surface *src, SDL_Surface *masque)
     return true ;
 }
 
+// Fonction: inverson
+// Synopsis: fonction permettant l'inversion de couleur sur une image
+// parm:
+// - *src, pointeur sur la surface à traiter
 void inversion(SDL_Surface *src)
 {
     Uint8 r, g, b ;
@@ -210,6 +279,13 @@ void inversion(SDL_Surface *src)
     SDL_UnlockSurface(src) ;
 }
 
+// Fonction: Rotate
+// Synopsis: fonction permettant la rotation de l'image de 180°
+// parm:
+// - *src, pointeur sur la surface à traiter
+// - dx, la hauteur de l'image
+// - dy, la largeur de l'image
+// - x et y servent au calcul de l'abcsisse et de l'ordonnée d'un pixel dans une moitié de l'image
 static
 void Rotate(SDL_Surface *src, int dx, int dy, int x, int y)
 {
@@ -228,6 +304,10 @@ void Rotate(SDL_Surface *src, int dx, int dy, int x, int y)
     SDL_UnlockSurface(src) ;
 }
 
+// Fonction: LeftRotate
+// Synopsis: fonction permettant la rotation de l'image vers le bas de 180°
+// parm:
+// - *src, pointeur sur la surface à traiter
 void UpRotate(SDL_Surface *src)
 {
 /*  Uint32 pixelH, pixelB ;
@@ -244,6 +324,10 @@ void UpRotate(SDL_Surface *src)
     Rotate(src, src->h/2, src->w, 0, src->h-1) ;
 }
 
+// Fonction: LeftRotate
+// Synopsis: fonction permettant la rotation de l'image vers la droite de 180°
+// parm:
+// - *src, pointeur sur la surface à traiter
 void LeftRotate(SDL_Surface *src)
 {
 /*  Uint32 pixelG, pixelD ;
@@ -262,10 +346,37 @@ void LeftRotate(SDL_Surface *src)
     Rotate(src, src->h, src->w/2, src->w, 0) ;
 }
 
+/*
 static
-Uint32 convolution(Uint32 (*Matrix)[3], Uint32 (*kernel)[3])
+void normalise(Uint32 (*matrix)[3], SDL_Surface *src, int n)
+{
+    if(src == NULL)
+        return ;
+
+    Uint8 r, g, b ;
+    Uint8 pixel ;
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<3; j++)
+        {
+            SDL_GetRGB(matrix[i][j], src->format, &r, &g, &b) ;
+            pixel = (r+g+b)/3 ;
+            matrix[i][j] = pixel ;
+        }
+    }
+}
+
+
+static
+Uint8 convolution(Uint32 (*Matrix)[3], Uint32 (*kernel)[3], SDL_Surface *src, int n)
 {
     Uint32 res = 0 ;
+
+    if(src == NULL)
+        return 0 ;
+
+    normalise(Matrix, src, n) ;
+
     for(int i = 0; i<3; i++)
     {
         for(int j = 0; j<3; j++)
@@ -273,17 +384,17 @@ Uint32 convolution(Uint32 (*Matrix)[3], Uint32 (*kernel)[3])
             res += Matrix[i][j]*kernel[i][j] ;
         }
     }
-   // printf("res %d\n", res) ;
-    return res ;
+   printf("res %d\n", res) ;
+    return (Uint8) res ;
 }
-
-
 
 SDL_Surface *flou(SDL_Surface *src)
 {
     Uint32 NewPixel ;
-    Uint8 RGB[3] = {0} ;
-    int RGB_Final[3] = {0} ;
+    Uint8 NewRGB ;
+    Uint8 r, g, b ;
+    int RGB_Final = 0 ;
+
 
     Uint32 kernel[3][3] = {{0, 1, 0},
                           {0, 0, 0},
@@ -294,8 +405,10 @@ SDL_Surface *flou(SDL_Surface *src)
     SDL_Surface *tmp = SDL_CreateRGBSurface(SDL_SWSURFACE, src->w, src->h, src->format->BitsPerPixel,
                                     src->format->Rmask, src->format->Gmask, src->format->Bmask, src->format->Amask) ;
 
-    if(tmp == NULL)
+    if(tmp == NULL || src == NULL)
         return NULL ;
+
+    //SDL_BlitSurface(src, NULL, tmp, NULL) ;
 
     SDL_LockSurface(tmp) ;
     SDL_LockSurface(src) ;
@@ -304,25 +417,14 @@ SDL_Surface *flou(SDL_Surface *src)
         for(int j = 0; j<src->h; j++)
         {
             MatIm[0][1] = getPixel(src, j-1, i-1); MatIm[0][2] = getPixel(src, j, i-1); MatIm[0][3] = getPixel(src, j, i-1) ;
-            MatIm[1][1] = getPixel(src, j-1, i);   MatIm[1][2] = getPixel(src, j, i);   MatIm[1][3] = getPixel(src, j+1, i) ;
+            MatIm[1][1] = getPixel(src, j-1, i);   MatIm[1][2] = getPixel(src, 0, 0);   MatIm[1][3] = getPixel(src, j+1, i) ;
             MatIm[2][1] = getPixel(src, j-1, i-1); MatIm[2][2] = getPixel(src, j, i+1); MatIm[2][3] = getPixel(src, j+1, i+1);
 
-        //NewPixel = convolution(MatIm, kernel) ;
-            for(int k = 0; k<3; k++)
-            {
-                for(int l = 0; l<3; l++)
-                {
-                    SDL_GetRGB(MatIm[k][l], src->format, &RGB[0], &RGB[1], &RGB[2]) ;
-                    RGB_Final[0] += RGB[0] ;
-                    RGB_Final[1] += RGB[2] ;
-                    RGB_Final[2] += RGB[1] ;
-                }
-            }
-            RGB_Final[0] /= 9 ;
-            RGB_Final[2] /= 9 ;
-            RGB_Final[1] /= 9 ;
-            NewPixel = SDL_MapRGB(tmp->format, RGB_Final[0], RGB_Final[2], RGB_Final[1]) ;
-            setPixel(tmp, j, i, NewPixel) ;
+            NewRGB = convolution(MatIm, kernel, src, 3) ;
+
+            NewPixel = SDL_MapRGB(src->format, NewRGB, NewRGB, NewRGB) ;
+            setPixel(src, j, i, NewPixel) ;
+            //
         }
     }
 
@@ -331,31 +433,71 @@ SDL_Surface *flou(SDL_Surface *src)
 
     return tmp ;
 }
+*/
 
-void contraste(SDL_Surface *src, int _c)
+// Fonction: detection
+// Synopsis: fonction permettant le detection de bord sur une image
+// Renvoie un pointeur NULL (nullptr) si la source vaut NULL (nullptr) ou
+//         si le pointeur sur la surface finale vaut NULL (nullptr) sinon
+//         un pointeur sur la surface finale
+// parm:
+// - *src La surface à traiter
+SDL_Surface *detection(SDL_Surface *src)
 {
-    // pixel.rouge = (pixel.rouge + valeur a ajouter / 100 * (pixel.rouge-127));
-    Uint32 pixel ;
+    Uint32 voisinnage[4] = {0} ;
+    Uint8 pixol[4] = {0} ;
     Uint8 r, g, b ;
+    Uint32 l, pixel ;
+
+    if(src == NULL)
+        return NULL ;
+
+    SDL_Surface *tmp = SDL_CreateRGBSurface(SDL_SWSURFACE, src->w, src->h, src->format->BitsPerPixel, src->format->Rmask, src->format->Gmask, src->format->Bmask, src->format->Amask) ;
+    if(tmp == NULL || src == NULL)
+        return NULL ;
+
+    SDL_BlitSurface(src, NULL, tmp, NULL) ;
 
     SDL_LockSurface(src) ;
-
+    SDL_LockSurface(tmp) ;
     for(int i = 0; i<src->h; i++)
     {
         for(int j = 0; j<src->w; j++)
         {
-            pixel = getPixel(src, j, i) ;
-            SDL_GetRGB(pixel, src->format, &r, &g, &b) ;
-            r = addVal(r, _c)/100*addVal(r, -127) ;
-            b = addVal(b, _c)/100*addVal(b, -127) ;
-            g = addVal(g, _c)/100*addVal(g, -127) ;
-            pixel = SDL_MapRGB(src->format, r, g, b) ;
-            setPixel(src, j, i, pixel) ;
+            voisinnage[0] = getPixel(src, j+1, i) ;
+            voisinnage[1] = getPixel(src, j, i+1) ;
+            voisinnage[2] = getPixel(src, j-1, i) ;
+            voisinnage[3] = getPixel(src, j, i-1) ;
+            for(int k = 0; k<4; k++)
+            {
+                SDL_GetRGB(voisinnage[k], src->format, &r, &g, &b) ;
+                pixol[k] = (r+g+b)/3 ;
+            }
+            l = (pixol[0]-pixol[2])*(pixol[0]-pixol[2]) + (pixol[1]-pixol[3])*(pixol[1]-pixol[3]) ;
+            //printf("%u\n", l) ;
+            l = SDL_sqrt(l) ;
+            //printf("%u\n", l) ;
+            if(l == 0)
+            {
+                pixel = SDL_MapRGB(tmp->format, 0, 0, 0) ;
+            }
+            else
+                pixel = SDL_MapRGB(tmp->format, l, l, l) ;
+
+            setPixel(tmp, j, i, pixel) ;
         }
     }
     SDL_UnlockSurface(src) ;
+    SDL_UnlockSurface(tmp) ;
+
+    return tmp ;
 }
 
+// Fonction: error
+// Synopsis: fonction vérifiant si un pointeur vaut NULL (nullptr) ou pas
+// SDL_GetError permet de déterminer l'erreur dans le cas d'une structure SDL
+// parm:
+// - *ptr le pointeur à checker
 bool error(void *ptr)
 {
     if(!ptr)
